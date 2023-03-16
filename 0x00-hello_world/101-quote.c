e5e59e701b04b883e1c47291298daad51d032542
Reechad300 <arubuogurichard@gmail.com>
2023-03-16 06:12:19 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - unix program
+ * Return: 1 (successful)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
