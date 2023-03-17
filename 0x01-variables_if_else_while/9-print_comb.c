63a3f896ef23ca86d3c8ff98457821a195e1d5cd
Reechad300 <arubuogurichard@gmail.com>
2023-03-17 10:11:14 -0700

    3p

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i <= 10 ; i++)
 	{
 		putchar(i + '0');
-			if (i != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
