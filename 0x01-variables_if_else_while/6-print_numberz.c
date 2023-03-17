f3c6643ba03cd727b71d9cc01ca3b2a6a5e13600
Reechad300 <arubuogurichard@gmail.com>
2023-03-17 08:44:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers with putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
