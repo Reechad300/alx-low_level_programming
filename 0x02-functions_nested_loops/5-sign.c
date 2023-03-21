cd830b208f2359a11d4b4b73d59ec43730c3f1ab
Reechad300 <arubuogurichard@gmail.com>
2023-03-21 09:21:37 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - print signs
+ * @n: print parameter
+ * Return: print 0 , 1 , -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(' ');
+		return (-1);
+
+	}
+}
