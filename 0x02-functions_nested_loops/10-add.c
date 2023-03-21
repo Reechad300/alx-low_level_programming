d9c0f91840011c14cb6dba83ee9fa59f7cedbd28
Reechad300 <arubuogurichard@gmail.com>
2023-03-21 10:53:32 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: para 1
+ * @b: para 2
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - print 9x table
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	
+}
