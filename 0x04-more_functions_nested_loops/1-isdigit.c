44a0c5b6e4457ace4fcb1fcc6e3f9a841ee754ff
Reechad300 <arubuogurichard@gmail.com>
2023-03-23 04:29:45 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - print digits 0 to 9
+ * @i: parameter
+ * Return: always 0
+ */
+
+int _isdigit(int i)
+{
+	if (i >= 0 && i <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
