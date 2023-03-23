15c7ba0f7c3bb80b7bb10dad0483aa789ac68459
Reechad300 <arubuogurichard@gmail.com>
2023-03-23 05:05:56 -0700

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#define MAIN_H
-#ifndef MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +9,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
-#endif
