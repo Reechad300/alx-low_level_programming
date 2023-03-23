0eac5cdfe98c082d2f58ca1e6c273b56973cc94b
Reechad300 <arubuogurichard@gmail.com>
2023-03-23 04:08:39 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper(int c) - print upper case alphabets
+ * Return: always 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
