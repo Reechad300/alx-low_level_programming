943ea82768b9b4ab6620cc9a8a37d2f822909ea9
Reechad300 <arubuogurichard@gmail.com>
2023-03-29 07:32:26 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - print upper case
+ * @str: parameter 1
+ * Return: nothing
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
