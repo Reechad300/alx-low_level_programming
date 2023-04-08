b2692c2a8f2f72c1f6f463db69bfa8c7c69fe46d
Reechad300 <arubuogurichard@gmail.com>
2023-04-08 02:41:23 -0700

    task 0

---
+++ 0x0A-agrc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
