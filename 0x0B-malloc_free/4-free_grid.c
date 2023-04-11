8f6df4bc20034466cbfc297ec4a83d370e767694
Reechad300 <arubuogurichard@gmail.com>
2023-04-11 10:15:01 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free grid
+ * @grid: parameter 1
+ * @height: parameter 2
+ * Return: always 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
